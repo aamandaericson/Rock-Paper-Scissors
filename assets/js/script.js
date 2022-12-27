572e056d025d18fcad7b012a8c91f5041d435a07
aamandaericson <amandaericson1@hotmail.com>
2022-12-27 09:11:06 +0000

    Fix images showing the right way when pressing the right button

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ const resetButton = document.getElementById('reset')
 const playerImg = document.getElementById("player-image")
 const computerImg = document.getElementById('computer-image')
 
-// Undefined variablkes for later use
+// Undefined variables for later use
 let userChoice;
 let computerChoice;
 let result;
@@ -16,14 +16,10 @@ let oldComputerScore;
 let oldYourScore;
 let yourNewScore = document.getElementById("your-score")
 let computerNewScore = document.getElementById("computer-score")
-const differentChoices = ['rock', 'paper, scissors']
-
-
-
-
 
 //Wait for the DOM to finnish loading before start
 //Get the button elements by class name and and add event listeners to them
+// Display user choice
 
 document.addEventListener("DOMContentLoaded", init());
 
@@ -32,33 +28,34 @@ function init() {
         possibleChoice.addEventListener("click", function() {
             userChoice = this.getAttribute("data-type"); 
             userChoiceDisplay.innerHTML = userChoice;
-            playGame()
             generateComputerChoice()
             getResult();
+            resetGame()
             });
         };
     };
 
-function playGame () {
-    if (computerChoice ='rock') {
-        computerImg.src = `assets/images/rock.jpg`
-    } else if (computerChoice ='paper') {
-        computerImg.src = `assets/images/paper.jpg`
-    }
-
-}
-
-
+    /**
+     * Generate the computer choice and 
+     * display according image
+     */
 function generateComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3) + 1; // + 1 for readability and you can also use .length
     if (randomNumber === 1) {
         computerChoice = 'rock'
+        computerImg.src = 'assets/images/rock.jpg'
+        computerImg.alt = 'Rock'
     }
     if (randomNumber === 2) {
         computerChoice = 'paper'
+        computerImg.src = 'assets/images/paper.jpg'
+        computerImg.alt = 'Rock'
     }
     if (randomNumber === 3) {
         computerChoice = 'scissors'
+        computerImg.src = 'assets/images/scissors.jpg'
+        computerImg.alt = 'Scissors'
+
     }
     computerChoiceDisplay.innerHTML = computerChoice;
 
@@ -66,7 +63,10 @@ function generateComputerChoice() {
 
 
 
-
+/**
+ * Get results in different game scenarios
+ * and display round result
+ */
 function getResult() {
 
     if (computerChoice === userChoice) {
@@ -74,39 +74,52 @@ function getResult() {
     } else if (computerChoice === 'rock' && userChoice ==='paper') {
         result = 'Paper beats rock! You get a point!'
         incrementScore()
+        //displayPaper()
     } else if (computerChoice === 'rock' && userChoice ==='scissors') {
         result = 'Rock beats scissors! Computer get a point!'
         incrementComputerScore()
+        //displayScissors()
     } else if (computerChoice === 'paper' && userChoice ==='scissors') {
         result = 'Scissors beats paper! You get a point!'
         incrementScore()
+        //displayScissors()
     } else if (computerChoice === 'paper' && userChoice ==='rock') {
         result = 'Paper beats rock! Computer get a point!'
+        //displayRock()
         incrementComputerScore()
     } else if (computerChoice === 'scissors' && userChoice ==='rock') {
         result = 'Rock beats scissors! You get a point!'
         incrementScore()
+        //displayRock()
     } else if (computerChoice === 'scissors' && userChoice ==='paper') {
         result = 'scissors beats paper! Computer get a point!'
         incrementComputerScore()
+        //displayPaper()
     } else {
         alert(`Choose Rock, Paper or scissors!`);
         throw `unidentified choice`;
     }
 
     resultDisplay.innerHTML = result  
+    showImage ()
 } 
 
+/**
+ * Display user image depending on userchoice
+ */
 function showImage (){
     if (userChoice === 'rock') {
         playerImg.src = `assets/images/rock.jpg`;
-        console.log('hej')
+    } else if (userChoice === 'paper') {
+        playerImg.src = `assets/images/paper.jpg`;
+    } else {
+        playerImg.src = `assets/images/scissors.jpg`;
     }
 }
-/* Detta är taget från Love maths (increment score) och sen har jag skrivit kod för att alerta om att datorn vunnit. Behöver här ha möjlighet att starta om spelet när detta nås. 
- Kanske typ sätta upp en div istället för alert som meddelar om att man vinner. Kanske färga grön eller röd beroende på vinst eller förlust och sedan kanske en knapp för att starta om. Kanske en bra ide att ha
-en omstartsknapp hur som helst så att man när som helst kan starta om. Detta gör ju då att jag måste attackera knapparna på annat sätt i början för att kunna ha ännu en knapp vilket ju är vad jag vill eftersom 
-det annars blir en rak kopia av youtube videon. Så kolla igen hur jag når knapparna på annat vis. Kan jag använda en submit istället för att starta om? Kanske inte men jag vill ju hur som helst nå på annat vis. */
+/**
+ * Increment current score and set it to a maximum of 5 points.
+ * Display a formerly hidden div that congratulate you if you win.
+ */
 function incrementScore() {
     if (yourNewScore.innerText < 5) {
         oldYourScore = parseInt(document.getElementById("your-score").innerText);
@@ -115,7 +128,6 @@ function incrementScore() {
 
 
     if (oldYourScore === 5) { 
-        resetGame()
         winLose = document.getElementById('win-or-lose')
         winLose.innerText = 'Congratulations! You won! Press the restart game button to go again!';
         hiddenDiv = document.getElementById('hidden-div');
@@ -124,7 +136,12 @@ function incrementScore() {
     }
 
 }
-// Detta är taget från Love maths (increment score) och sen har jag skrivit kod för att alerta om att datorn vunnit. Behöver här ha möjlighet att starta om spelet när detta nås.
+
+/**
+ * Increment current score and set it to a maximum of 5 points.
+ * Display a formerly hidden div that congratulate you if you win.
+ */
+
 function incrementComputerScore() {
     if (computerNewScore.innerText < 5) {
         oldComputerScore = parseInt(document.getElementById("computer-score").innerText);
@@ -133,7 +150,6 @@ function incrementComputerScore() {
 
 
     if (oldComputerScore === 5) {
-        resetGame()
         winLose = document.getElementById('win-or-lose')
         winLose.innerText = 'You Lost! Press the restart game button to go again!';
         hiddenDiv = document.getElementById('hidden-div');
@@ -145,7 +161,9 @@ function incrementComputerScore() {
     
 }
 
-// Reset game
+ /**
+  * Add event listener to reset button and sets computer and user score to 0. Resets the game.
+  */
 
 function resetGame() {
     resetButton.addEventListener('click', function(){
@@ -155,6 +173,15 @@ function resetGame() {
     document.getElementById("computer-score").innerText = 0
     yourNewScore.innerText = 0
     })
+
+    removeWinLose()
     };
+
+function removeWinLose() {
+    winLose = document.getElementById('win-or-lose')
+    winLose.innerText = '';
+    hiddenDiv = document.getElementById('hidden-div');
+    hiddenDiv.style.backgroundColor = '';
+}
 
 
